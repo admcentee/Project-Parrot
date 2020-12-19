b42381854a52c51da04e03b2b68848e6e43205c9
amcentee <amcentee@mymail.mines.edu>
2020-12-19 17:32:31 -0500

    Deleted unused files and added basic test level.

---
+++ Assets/Player/PlayerLook.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera cam;
+    public float unZoomedSensitivity = 150f;
+    public float FOV = 110f;
+    private bool locked = false;
+    float xRotation = 0f;
+    float yRotation = 0f;
+    private float lookSensitivity = 0f;
+    private bool initalized = true;
+
+
+    void Start()
+    {
+        lookSensitivity = unZoomedSensitivity;
+        // cam = GameObject.FindGameObjectWithTag("PlayerCamera").GetComponent<Camera>();
+        GetComponent<Rigidbody>().useGravity = false;
+        locked = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        //Keep cursor confined to game window
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (locked) { locked = false; Cursor.lockState = CursorLockMode.None; Cursor.visible = true; }
+            else { locked = true; Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false; }
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Collider col = GetComponent<Collider>();
+            if (col.enabled) col.enabled = false;
+            else col.enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (GetComponent<Rigidbody>().useGravity) GetComponent<Rigidbody>().useGravity = false;
+            else GetComponent<Rigidbody>().useGravity = true;
+        }
+        if (locked && initalized)
+        {
+            // GetComponent<Rigidbody>().freezeRotation = false;
+            // cam.fieldOfView = FOV;
+            float mouseX = Input.GetAxis("Mouse X") * lookSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * lookSensitivity * Time.deltaTime;
+
+            // Debug.Log(mouseX + " - " + mouseY);
+
+            xRotation -= mouseY;    //Vertical
+            yRotation += mouseX;    //Horizontal
+
+            //Clamp vertical FOV to +-85 degrees
+            xRotation = Mathf.Clamp(xRotation, -85f, 85f);
+            //Rotate camera vertically
+            cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            //Rotate body horizontally
+            transform.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+
+    }
+    public void Zoom(bool state)
+    {
+        if (state)
+        {
+            cam.fieldOfView = FOV / 2f;
+            lookSensitivity = unZoomedSensitivity / 5f;
+        }
+        else
+        {
+            cam.fieldOfView = FOV;
+            lookSensitivity = unZoomedSensitivity;
+        }
+    }
+    public void Initialize()
+    {
+        initalized = true;
+    }
+
+}
---
+++ Assets/Player/RigidbodyFPSController.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RigidbodyFPSController : MonoBehaviour
+{
+
+    public float walkSpeed = 20f;
+    public float sprintMultiplier = 1.75f;
+    public float maxVelocity = 100;
+    public float jumpVelocity = 50f;
+    public float jumpRate = 1f;
+    public int jumpLimit = 2;
+    public float jumpDecayWeight = 0.75f;
+    public Transform groundCheck;
+    public float groundDistance = 0.25f;
+    public LayerMask playerMask;
+    public LayerMask groundMask;
+    public AudioClip walk;
+    public AudioClip landing;
+
+    // public AudioClip sliding;
+
+    private Rigidbody rb;
+    private float x = 0;
+    private float z = 0;
+    private bool sprintFlag = false;
+    private bool jumpFlag = false;
+    private bool grounded = false;
+    private AudioSource audioSource;
+    private float curSpeed;
+    private float jumpCooldownCounter = 0f;
+
+    private new Collider collider;
+    private bool pulled = false;
+    private bool active = false;
+    private bool initialized = true;
+    private Vector3 lastDir = Vector3.zero;
+    // private float elapsedTime = 0;
+    private int jumpCounter = 0;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        collider = GetComponent<Collider>();
+        rb.useGravity = false;
+    }
+    void Update()
+    {
+        if (jumpCooldownCounter >= 0) jumpCooldownCounter -= Time.deltaTime;
+    }
+    void FixedUpdate()
+    {
+            if (!active && initialized)
+            {
+                StartCoroutine(processInputs());
+            }
+    }
+
+    IEnumerator processInputs()
+    {
+        // elapsedTime = 0;
+        active = true;
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        sprintFlag = Input.GetKey(KeyCode.LeftShift);
+        jumpFlag = Input.GetKey(KeyCode.Space);
+        float y = 0f;
+        if (Input.GetKey(KeyCode.Space) && !rb.useGravity) y = 1f;
+        else if (Input.GetKey(KeyCode.X) && !rb.useGravity) y = -1f;
+
+        yield return null;
+
+        // Determine walk speed
+        curSpeed = walkSpeed;
+        if (sprintFlag) curSpeed *= sprintMultiplier;
+
+        // Check for ground contact
+
+        // RaycastHit[] collisions;
+        // if(Physics.SphereCast(groundCheck.position, groundDistance, -Vector3.up, out collisions, 0, ~gameObject.layer));
+        // Collider[] cols = Physics.OverlapSphere(groundCheck.position, groundDistance, ~playerMask);
+        bool temp = Physics.CheckSphere(groundCheck.position, groundDistance, ~playerMask);
+        // Vector3 normal = (groundCheck.position - cols[0].ClosestPoint(groundCheck.position)).normalized;
+
+        // Play landing on change
+        if (temp != grounded)
+        {
+            if (temp && landing != null)
+            {
+                audioSource.volume = 0.75f;
+                audioSource.pitch = 1f;
+                audioSource.PlayOneShot(landing);
+            }
+            // else
+            // {
+            //     audioSource.Stop();
+            // }
+
+        }
+        grounded = temp;
+        if (grounded) jumpCounter = 0;
+        else if (audioSource != null) audioSource.Stop();
+
+
+        Vector3 curDir;
+        curDir = x * transform.right + y * transform.up + z * transform.forward;
+        curDir *= curSpeed * Time.fixedDeltaTime;
+
+        // Player Mode
+        if (rb.useGravity)
+        {
+            // Jump if applicable
+
+            if (jumpFlag && jumpCooldownCounter <= 0 && jumpCounter < jumpLimit)
+            {
+                if (!grounded && jumpCounter == 0) jumpCounter = 1;
+                jumpCounter++;
+                jumpCooldownCounter = 1f / jumpRate;
+                Jump(Vector3.up, jumpCounter);
+                yield return null;
+            }
+
+            // Moving
+            if (x != 0 || z != 0)
+            {
+                RaycastHit destination;
+                //* curDir.magnitude / curSpeed
+                if (grounded && Physics.Raycast(transform.position + curDir + transform.up * collider.bounds.size.y * 1.5f, -transform.up, out destination, collider.bounds.size.y * 3f, ~playerMask))
+                {
+                    MovePos(destination.point, sprintFlag);
+
+                    yield return null;
+                }
+                else if (!Physics.Raycast(transform.position, curDir.normalized, curDir.magnitude))
+                {
+                    MoveDir(curDir / 1.5f, sprintFlag);
+                }
+
+            }
+
+            // Standing still
+            else if (grounded || (pulled && !grounded))
+            {
+                if (grounded && !pulled) rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, 10f * Time.fixedDeltaTime);
+                if (audioSource != null) audioSource.Stop();
+            }
+            if (rb.velocity.magnitude > maxVelocity) rb.velocity = Vector3.Lerp(rb.velocity, rb.velocity.normalized * maxVelocity, 20f * Time.deltaTime);
+
+        }
+        // Admin Mode
+        else
+        {
+            // Moving
+            if ((x != 0 || y != 0 || z != 0))
+            {
+                MoveDir(curDir, sprintFlag);
+            }
+            else if (!jumpFlag && audioSource != null)
+            {
+                audioSource.Stop();
+            }
+        }
+        active = false;
+        yield break;
+    }
+    public void Initialize()
+    {
+        initialized = true;
+    }
+    void MoveDir(Vector3 input, bool sf)
+    {
+        // Pitch footsteps according to walk speed
+        if (audioSource != null){
+            float pitchMod = rb.velocity.magnitude / 25f;
+            if (pitchMod > sprintMultiplier) audioSource.pitch = Mathf.Clamp(rb.velocity.magnitude / 25f, 1f, sprintMultiplier * 2);
+            else if (sf) audioSource.pitch = sprintMultiplier;
+            else audioSource.pitch = 1f;
+        }
+        // yield return null;
+        if (audioSource != null && walk != null)
+        {
+            if (!audioSource.isPlaying && grounded) audioSource.PlayOneShot(walk);
+        }
+
+        rb.MovePosition(transform.position + input);
+    }
+    void MovePos(Vector3 destination, bool sf)
+    {
+        // Pitch footsteps according to walk speed
+        float pitchMod = rb.velocity.magnitude / 25f;
+        if (pitchMod > sprintMultiplier) audioSource.pitch = Mathf.Clamp(rb.velocity.magnitude / 25f, 1f, sprintMultiplier * 2);
+        else if (sf) audioSource.pitch = sprintMultiplier;
+        else audioSource.pitch = 1f;
+        // yield return null;
+        if (audioSource != null && walk != null)
+        {
+            if (!audioSource.isPlaying && grounded) audioSource.PlayOneShot(walk);
+        }
+        rb.MovePosition(destination);
+    }
+    void Jump(Vector3 normal, int jumpCounter)
+    {
+        float vel = jumpVelocity / (jumpDecayWeight * jumpCounter);
+        if (jumpCounter == 1) vel = jumpVelocity;
+        if (pulled)
+        {
+            rb.AddForce(normal * vel, ForceMode.VelocityChange);
+            grounded = false;
+        }
+        else
+        {
+            rb.AddForce(Vector3.up * vel, ForceMode.VelocityChange);
+            grounded = false;
+        }
+
+    }
+    public void setPullState(bool state)
+    {
+        pulled = state;
+    }
+}
+
